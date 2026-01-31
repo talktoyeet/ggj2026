6d530ff6393db9682e6e305afe9d58be9c591cfa
talktoyeet <83469956+talktoyeet@users.noreply.github.com>
2026-01-31 16:54:15 +0800

    added some new pattern

--- ggjhk-26/Scripts/boss.gd
+++ ggjhk-26/Scripts/boss.gd
@@ -5,10 +5,10 @@ const BULLET_SCENE: PackedScene = preload("res://Scenes/Bullet.tscn")
 @onready var shoot_timer: Timer = $ShootTimer
 @onready var rotator: Node2D = $Rotator
 
-const ROTATE_SPEED: float = 100.0
+const ROTATE_SPEED: float = 0
 const SHOOTER_TIMER_WAIT_TIME: float = 0.2
 const SPAWN_POINT_COUNT: int = 4
-const RADIUS: float = 100.0
+const RADIUS: float = 10
 
 func _ready() -> void:
 	# create spawn points in a circle
@@ -33,7 +33,6 @@ func _process(delta: float) -> void:
 
 
 func _on_ShootTimer_timeout() -> void:
-	print("shooting")
 	for s in rotator.get_children():
 		var bullet := BULLET_SCENE.instantiate()
 		# better to add to a game/arena node instead of root, but this works:
--- ggjhk-26/Scripts/bullet.gd
+++ ggjhk-26/Scripts/bullet.gd
@@ -4,6 +4,18 @@ const speed = 100
 var lifetime = 10
 @onready var timer: Timer = $Timer
 
+enum BulletType { SMALL, BIG, LASER }
+
+func _setup_bullet(type: BulletType):
+	var atlas = $Sprite2D.texture as AtlasTexture
+	match type:
+		BulletType.SMALL:
+			atlas.region = Rect2(0, 0, 8, 8)
+		BulletType.BIG:
+			atlas.region = Rect2(8, 0, 12, 12)
+		BulletType.LASER:
+			atlas.region = Rect2(0, 8, 24, 6)
+
 func _process(delta: float) -> void:
 	position += transform.x * speed * delta
 	lifetime -= delta
